05c83445cd050bfe2a736e0b04de9605e5d055d4
David Fulton <david.fulton@lifestylepubs.com>
2020-12-31 07:48:28 -0600

    Added husky

---
+++ gridsome.configer.js
@@ -0,0 +1,101 @@
+module.exports = {
+  siteName: 'theDaveFulton',
+  siteDescription: 'A blog by the Dave Fulton',
+  siteUrl: 'https://thedavefulton.com',
+  titleTemplate: `%s | theDaveFulton`,
+  icon: 'src/favicon.png',
+
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        [
+          'gridsome-plugin-remark-shiki',
+          {
+            theme: 'min-light'
+          }
+        ]
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true
+          },
+          author: {
+            typeName: 'Author',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-186276179-1'
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000 // default
+      }
+    },
+    {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'A blog by the Dave Fulton',
+          feed_url: 'https://thedavefulton.com/feed.xml',
+          site_url: 'https://thedavefulton.com'
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          description: node.description,
+          url: 'https://thedavefulton.com' + node.path,
+          author: node.author,
+          date: node.date
+        }),
+        output: {
+          dir: './static',
+          name: 'feed.xml'
+        }
+      }
+    }
+  ],
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id',
+    Author: '/author/:id'
+  },
+
+  chainWebpack: config => {
+    config.module
+      .rule('css')
+      .oneOf('normal')
+      .use('postcss-loader')
+      .tap(options => {
+        options.plugins.unshift(
+          ...[
+            require('postcss-import'),
+            require('postcss-nested'),
+            require('tailwindcss')
+          ]
+        );
+
+        return options;
+      });
+  }
+};
